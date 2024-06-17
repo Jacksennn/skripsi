bf92db56a1d50f70d2ef773f082a29eacb3d0085
Jacksen <senjack18@gmail.com>
2024-06-17 14:10:49 +0700

    refine navbar add product adjsutment link

--- src/components/widget/navigation-bar/index.tsx
+++ src/components/widget/navigation-bar/index.tsx
@@ -1,5 +1,7 @@
 import {
+  ArrowDown,
   BellSimple,
+  CaretDown,
   Gear,
   Heart,
   NotePencil,
@@ -12,13 +14,14 @@ import {
   Users,
 } from "@phosphor-icons/react";
 import { Flex } from "antd";
-import { useRouter } from "next/router";
-import React from "react";
+import { NextRouter, Router, useRouter } from "next/router";
+import React, { useState } from "react";
 import { navigationBarStyles } from "./styles.css";
 import { colors } from "@/theming/colors";
 
+type Route = { name: string; label: string; icon: React.ReactNode };
 const route: {
-  [key: string]: { name: string; label: string; icon: React.ReactNode };
+  [key: string]: Route | (Route & { items: Route[] });
 } = {
   dashboard: {
     name: "dashboard",
@@ -34,6 +37,18 @@ const route: {
     name: "product",
     label: "Product",
     icon: <Storefront size={20} />,
+    items: [
+      {
+        name: "product",
+        label: "Product",
+        icon: <Storefront size={20} />,
+      },
+      {
+        name: "product-adjustment",
+        label: "Product Adjustment",
+        icon: <Storefront size={20} />,
+      },
+    ],
   },
   category: { name: "category", label: "Category", icon: <Tag size={20} /> },
   supplier: { name: "supplier", label: "Supplier", icon: <Truck size={20} /> },
@@ -57,31 +72,83 @@ const route: {
   setting: { name: "settings", label: "Setting", icon: <Gear size={20} /> },
 };
 
+function NavItem(props: Route & { router: NextRouter }) {
+  const { router, name, icon, label } = props;
+
+  return (
+    <Flex
+      className={navigationBarStyles.item}
+      gap={12}
+      {...(router.pathname.split("/")?.[2] === name && {
+        style: {
+          color: colors.gray00,
+          backgroundColor: colors.secondary800,
+          fontWeight: 600,
+        },
+      })}
+      onClick={() => router.push(`/admin/${name}`)}
+    >
+      {icon}
+      <div className={navigationBarStyles.iconContainer}>{label}</div>
+    </Flex>
+  );
+}
+
+function NavWithItems(props: Route & { items: Route[]; router: NextRouter }) {
+  const { router, name, items, icon, label } = props;
+  const [isOpen, setIsOpen] = useState<boolean>(router.pathname.includes(name));
+
+  return (
+    <>
+      <Flex
+        className={navigationBarStyles.item}
+        gap={12}
+        onClick={() => setIsOpen((prev) => !prev)}
+        flex={1}
+        style={{ width: "100%" }}
+      >
+        {icon}
+        <div className={navigationBarStyles.iconContainer}>{label}</div>
+        <CaretDown size={12} />
+      </Flex>
+      {isOpen &&
+        items.map((item) => (
+          <Flex
+            key={item.name}
+            className={navigationBarStyles.item}
+            gap={12}
+            style={{
+              marginLeft: 32,
+              borderLeft: `1px solid ${colors.gray100}`,
+              ...(router.pathname.split("/")?.[2] === item.name && {
+                color: colors.gray00,
+                backgroundColor: colors.secondary800,
+                fontWeight: 600,
+              }),
+            }}
+            onClick={() => router.push(`/admin/${item.name}`)}
+          >
+            <div className={navigationBarStyles.iconContainer}>
+              {item.label}
+            </div>
+          </Flex>
+        ))}
+    </>
+  );
+}
 export default function NavigationBar() {
   const router = useRouter();
 
   return (
     <div className={navigationBarStyles.container}>
-      {Object.keys(route).map((key: string) => (
-        <Flex
-          key={key}
-          className={navigationBarStyles.item}
-          gap={12}
-          {...(router.pathname.split("/")?.[2] === route[key].name && {
-            style: {
-              color: colors.gray00,
-              backgroundColor: colors.secondary800,
-              fontWeight: 600,
-            },
-          })}
-          onClick={() => router.push(`/admin/${route[key].name}`)}
-        >
-          {route[key].icon}
-          <div className={navigationBarStyles.iconContainer}>
-            {route[key].label}
-          </div>
-        </Flex>
-      ))}
+      {Object.keys(route).map((key: string) => {
+        const hsItem = Object.keys(route[key]).includes("items");
+        return hsItem ? (
+          <NavWithItems router={router} {...(route[key] as any)} />
+        ) : (
+          <NavItem router={router} {...route[key]} key={key} />
+        );
+      })}
     </div>
   );
 }
